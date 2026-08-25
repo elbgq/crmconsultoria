2269659f0e65d553f064cfc060e2ccc60d4e89e0
eloi lerner <eloibgq@gmail.com>
2026-08-25 19:12:25 -0300

    Alterações de estilos diferentes e opcionais do front-end

--- crm_core/views.py
+++ crm_core/views.py
@@ -7,7 +7,7 @@ from oportunidades.models import Oportunidade
 
 class HomeView(LoginRequiredMixin, TemplateView):
     template_name = 'crm_core/home.html'
-
+ 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         oportunidades_abertas = Oportunidade.objects.exclude(estagio__in=['ganho', 'perdido'])
